67e43cc374283f4cd6e8ccc1c774a31a6369f332
Dmytro Chernomord <tschernomord@gmail.com>
2020-07-10 17:34:38 +0200

    fixed button

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,16 @@
 ;(() => {
 	const menuBtnRef = document.querySelector("[data-menu-button]")
 	const mobileMenuRef = document.querySelector("[data-menu]")
+	const menuBtnClose = document.querySelector("[data-menu-close]")
 
 	menuBtnRef.addEventListener("click", () => {
 		menuBtnRef.classList.toggle("is-open")
 
 		mobileMenuRef.classList.toggle("is-open")
 	})
-})()(() => {
-	const refs = {
-		closeMenulBtn: document.querySelector("[data-menu-close]"),
-		modal: document.querySelector("[data-modal]"),
-	}
-	refs.closeMenuBtn.addEventListener("click", toggleModal)
-	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden")
-	}
+	menuBtnClose.addEventListener("click", () => {
+		menuBtnRef.classList.toggle("is-open")
+
+		mobileMenuRef.classList.toggle("is-open")
+	})
 })()
